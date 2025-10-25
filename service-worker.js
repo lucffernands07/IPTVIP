7c279d4f2690f5f875e0ff8234c3ed5eea433c7f
lucffernands07 <lucianoffernands@outlook.com.br>
2025-10-25 16:51:59 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const cacheName = 'iptvip-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
