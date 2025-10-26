4d7ca1ddaeeaa86b6c264732e53b03524cfc2f09
lucffernands07 <lucianoffernands@outlook.com.br>
2025-10-26 00:17:52 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nome do cache, atualize a cada versão do app
-const cacheName = 'iptvip-cache-v1.2.3';
+const cacheName = 'iptvip-cache-v1.2.4';
 
 // Arquivos que queremos manter no cache
 const assetsToCache = [
