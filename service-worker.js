5c2e57658332106af42e6f6c5bd78b8eeef8c489
lucffernands07 <lucianoffernands@outlook.com.br>
2025-10-29 14:45:38 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // 🔸 IPTV Player Service Worker
 // ============================
 
-const APP_VERSION = 'v1.6.10'; // 🔁 Atualize conforme necessário
+const APP_VERSION = 'v1.6.11'; // 🔁 Atualize conforme necessário
 const cacheName = `iptvip-cache-${APP_VERSION}`;
 
 const assetsToCache = [
